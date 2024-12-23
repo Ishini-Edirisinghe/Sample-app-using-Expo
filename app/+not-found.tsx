ba77a06e57949318b6c8ac65137ab8eb6d26de4b
Ishini-Edirisinghe <ishiniedirisinghe331@gmail.com>
2024-12-23 12:56:14 +0530

    added not found page and page navigation

---
+++ app/+not-found.tsx
@@ -0,0 +1,33 @@
+import { Link, Stack } from "expo-router";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+       <Stack.Screen  options={{
+    title:"Opps ! Not Found"}}
+  />
+      <View style={styles.container}>
+        <Link href={"/"} style={styles.button}>
+          Go Back to Home Screen !
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000000",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
