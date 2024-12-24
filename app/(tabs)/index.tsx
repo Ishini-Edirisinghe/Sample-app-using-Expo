34b66b5b53571b21869332415554311addc6f3c7
Ishini-Edirisinghe <ishiniedirisinghe331@gmail.com>
2024-12-24 11:59:30 +0530

    added image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,32 +1,35 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
-      <Text style={styles.text}>Hello there!</Text>
-  
+    <View style={styles.container}>
+      <Image source={PlaceholderImage} style={styles.image} />
     </View>
-    
   );
 }
 
-
-const styles =StyleSheet.create({
-  container:{
-    backgroundColor:"#FBCFCD",
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FBCFCD",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  text:{
-    color:"white",
-    fontWeight:"bold"
+  text: {
+    color: "white",
+    fontWeight: "bold",
   },
-  button:{
-    fontSize:20,
-    textDecorationLine:"underline",
-    color:"#fff",
-  }
-})
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+});
