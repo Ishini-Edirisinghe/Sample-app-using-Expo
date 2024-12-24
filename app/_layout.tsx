fb84c414f828fdf6745632c4d17b1393ea35dd14
Ishini-Edirisinghe <ishiniedirisinghe331@gmail.com>
2024-12-24 11:04:36 +0530

    status bar color style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,24 @@
 import { Stack } from "expo-router";
 import React from "react";
 import { LogBox } from "react-native";
+ import { StatusBar } from "expo-status-bar";
 
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
-  <Stack>
-  <Stack.Screen name="(tabs)"
-  options={{
-   headerShown:false,
-  }}
-  />
-  
-  <Stack.Screen name="+not-found"
-  options={{}}
-  /> 
-  </Stack>
+    <>
+    <StatusBar style="light" />
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen name="+not-found" options={{}} />
+      </Stack>
+    </>
   );
 }
