c7d695cf6ec7b77338d13bb3eebef80b95a478b2
Ishini-Edirisinghe <ishiniedirisinghe331@gmail.com>
2024-12-26 17:41:21 +0530

    add GestureHandlerRootView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,14 @@
 import { Stack } from "expo-router";
 import React from "react";
 import { LogBox } from "react-native";
- import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
-    <>
+    <GestureHandlerRootView  style={{flex:1}}>
     <StatusBar style="light" />
       <Stack>
         <Stack.Screen
@@ -19,6 +20,6 @@ export default function RootLayout() {
 
         <Stack.Screen name="+not-found" options={{}} />
       </Stack>
-    </>
+    </GestureHandlerRootView>
   );
 }
